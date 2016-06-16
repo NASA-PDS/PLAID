4129d2bc517fe0938ca952eb9e8807d3e15da96c
Trevor Morse <trevor.a.morse@jpl.nasa.gov>
2016-06-16 16:13:35 -0700

    ported over test code implementing jquery steps example

---
+++ js/init_steps.js
@@ -0,0 +1,58 @@
+/**
+ * Created by morse on 6/16/16.
+ */
+var settings = {
+    /* Appearance */
+    headerTag: "h3",
+    bodyTag: "section",
+    contentContainerTag: "div",
+    actionContainerTag: "div",
+    stepsContainerTag: "div",
+    cssClass: "wizard",
+    stepsOrientation: $.fn.steps.stepsOrientation.horizontal,
+
+    /* Templates */
+    titleTemplate: '<span class="number">#index#.</span> #title#',
+    loadingTemplate: '<span class="spinner"></span> #text#',
+
+    /* Behaviour */
+    autoFocus: false,
+    enableAllSteps: false,
+    enableKeyNavigation: true,
+    enablePagination: true,
+    suppressPaginationOnFocus: true,
+    enableContentCache: true,
+    enableCancelButton: true,
+    enableFinishButton: true,
+    preloadContent: false,
+    showFinishButtonAlways: false,
+    forceMoveForward: false,
+    saveState: false,
+    startIndex: 0,
+
+    /* Transition Effects */
+    transitionEffect: $.fn.steps.transitionEffect.none,
+    transitionEffectSpeed: 200,
+
+    /* Events */
+    /*onStepChanging: function (event, currentIndex, newIndex) { return true; },
+     onStepChanged: function (event, currentIndex, priorIndex) { }},
+     onCanceled: function (event) { },
+     onFinishing: function (event, currentIndex) { return true; },
+     onFinished: function (event, currentIndex) { },*/
+
+    /* Labels */
+    labels: {
+        cancel: "Cancel",
+        current: "-> ",
+        pagination: "Pagination",
+        finish: "Finish",
+        next: "Next",
+        previous: "Previous",
+        loading: "Loading ..."
+    }
+};
+
+function init_steps_object(wiz_object) {
+    wiz_object.steps(settings);
+}
