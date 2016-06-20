f9057339ebfcbe4c7ae369d0e7a1246f7a594eae
Trevor Morse <trevor.a.morse@jpl.nasa.gov>
2016-06-20 08:28:59 -0700

    minor changes

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,5 @@
  */
 //setup the wizard and sidebar for the web application
 var wizard = $("#wizard");
-var sidebar = $("#sidebar");
 init_steps_object(wizard);
-match_wizard_height(wizard[0], sidebar[0]);
+match_wizard_height(wizard[0], $("#sidebar")[0]);
