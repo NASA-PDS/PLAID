c75f0f5df150a4142037d4c509fa385cbe9052ff
Trevor Morse <trevor.a.morse@jpl.nasa.gov>
2016-08-09 15:55:09 -0700

    (issue #30) added session variables to prevent access of main page until successful login -added code for starting a session and maintaining session variables -renamed files

--- wizard.php
+++ wizard.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if (!isset($_SESSION['login']) && !$_SESSION['login'])
+    header("Location: login.html");
+$_SESSION['login'] = false;
+?>
 <!DOCTYPE html>
 <html>
 <head>
