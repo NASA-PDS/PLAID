0e624cdd79706215fcc7a936627ae1db1a6e1cea
Michael Kim <michael.y.kim@jpl.nasa.gov>
2016-08-17 15:46:34 -0700

    Editing documentation Gave yesFunctions proper function headers

--- config/pop_up_config.js
+++ config/pop_up_config.js
@@ -1,9 +1,25 @@
 /**
- * Created by mikim on 7/25/16.
+ * @file An isolated location to store configurations for the pop-up windows used throughout the LDT. The main intent
+ * of this file is to interact closely with the generatePopUp method found in pop_up.js, supplying it with
+ * the content and functions it requires to dynamically construct unique pop-ups for any given situation.
+ *
+ * Note: Many yesFunctions will redirect the window upon click in order to navigate the user to the next process.
+ * Note: The generatePopUp method does not need to use objects from this file to create pop-ups, the Objects can be
+ *       constructed anywhere. This comes in handy when a pop-up calls for content or functions that cannot be
+ *       defined statically.
+ *
+ * Creation Date: 7/25/16.
+ *
+ * @author Michael Kim
+ * @author Trevor Morse
  */
 var popUpData = {
+    // These variables, currentStep and newStep, are used to store the initial step change attempt made by the user,
+    // which is referenced again later in the pop-up's yesFunctions that have intent to mimic the same step change
     currentStep : 0,
     newStep : 0,
+    // When a user is leaving in the middle of constructing a single attribute, this pop-up warns the user that
+    // the progress made for this particular single attribute will not be saved
     addAttr : {
         id : "addAttr",
         title : "Warning",
@@ -11,6 +27,12 @@ var popUpData = {
         "<div>Click 'Continue' to leave anyway.</div>",
         noText : "Cancel",
         yesText : "Continue",
+        /**
+         * When a user confirms to leave out of this phase of the mission specifics builder, use jQuery to click the
+         * step on the left toolbar representing the desired new location. To leave the page without triggering
+         * the same pop-up once again, a leaving class is added to the HTML. This flag is processed in updatePopup of
+         * the pop_up.js file.
+         */
         yesFunction : function() {
             $('#addAttr').modal('hide');
             var section = $("#wizard-p-" + popUpData.currentStep.toString());
@@ -24,6 +46,8 @@ var popUpData = {
             $(newStep).click();
         }
     },
+    // When a user is leaving in the middle of constructing a group of attributes, this pop-up warns the user that
+    // the progress made for this particular group will not be saved
     addGroup : {
         id : "addGroup",
         title : "Warning",
@@ -31,6 +55,9 @@ var popUpData = {
         "<div>Click 'Continue' to leave anyway.</div>",
         noText : "Cancel",
         yesText : "Continue",
+        /**
+         * See doc for yesFunction in addAttr object
+         */
         yesFunction : function() {
             $('#addGroup').modal('hide');
             var section = $("#wizard-p-" + popUpData.currentStep.toString());
@@ -44,12 +71,17 @@ var popUpData = {
             $(newStep).click();
         }
     },
+    // When a user incorrectly fills out a choice section of optional PDS4-attributes, disallow the step change
+    // and force the user to complete the form correctly
     invalidChoice : {
         id : "invalidChoice",
         title : "Invalid Choice",
         content : "Please properly select the elements in the choice section.",
         noText: "",
         yesText: "OK",
+        /**
+         * Hides the pop-up once the user confirms that an error exists
+         */
         yesFunction : function() {
             $('#invalidChoice').modal('hide');
         }
@@ -62,6 +94,9 @@ var popUpData = {
         "<div>You are safe to navigate through the different steps without making any changes, however.</div>",
         noText : "",
         yesText : "OK",
+        /**
+         * Hides the pop-up once the user agrees to the warning of backwards traversal in the LDT wizard
+         */
         yesFunction : function() {
             var wrapper = $("#wizard-p-" + popUpData.currentStep.toString());
             $(wrapper).removeAttr("pop-up");
@@ -72,6 +107,8 @@ var popUpData = {
             })
         }
     },
+    // When a user attempts to create a new label from the dashboard, this pop-up forces the user to give the label
+    // a name before proceeding
     createNewLabel : {
         id : "createNewLabel",
         title : "Create New Label",
@@ -79,6 +116,9 @@ var popUpData = {
         "<input id='labelNameInput' class='form-control' type='text' placeholder='Ex. Mars 2020 Label' id='example-text-input'>",
         noText: "Cancel",
         yesText: "Submit",
+        /**
+         * Captures user's input for label name and sends it to the backend to be processed
+         */
         yesFunction : function() {
             if (isValidLabelNameInput($('#labelNameInput'))) {
                 $('#createNewLabel').modal('hide');
@@ -94,6 +134,8 @@ var popUpData = {
             }
         }
     },
+    // When a user attempts to change the label from a previously visited step, give the user the option to save
+    // the change and lose the proceeding progress or to revert the changes made
     deleteProgress : {
         id : "deleteProgress",
         title : "Warning",
@@ -102,6 +144,11 @@ var popUpData = {
             " If you revert, then the changes will be reverted and you will advance to the next step.</i></div>",
         noText : "Revert Changes",
         yesText : "Continue",
+        /**
+         * Removes the progressData proceeding this step and stores the new changes
+         * After, a backend call is made to remove root attributes
+         * Finally, the window is reloaded for a proper load of the newly formed progress
+         */
         yesFunction : function(){
             var type = progressData[wizardData.currentStep]['step'];
             progressData = progressData.slice(0, wizardData.currentStep);
@@ -112,16 +159,24 @@ var popUpData = {
                 function(data){});
             location.reload(true);
         },
+        /**
+         * Returns the user to the same step using the unchanged progressData
+         */
         noFunction : function(){
             loadProgress(progressData[wizardData.currentStep]);
         }
     },
+    // When a user wants to preview the
+    // Note: The content for this pop-up is set dynamically from where it calls the generate method
     preview: {
         id : "preview",
         title : "Label Template Preview",
         content : "",
         noText : "",
         yesText : "Dismiss",
+        /**
+         * Hides the pop-up
+         */
         yesFunction : function(){
             $('#preview').modal('hide');
         }
