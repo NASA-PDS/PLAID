e61b3ba432a8c05db5a8b4d010fadeb5eaa9e033
Michael Kim <michael.y.kim@jpl.nasa.gov>
2016-08-17 16:53:00 -0700

    Editing code documentation and adding new lines to ends of files

--- php/logout.php
+++ php/logout.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: mikim
- * Date: 8/10/16
- * Time: 2:09 PM
+ * @file Sets the login field in a session variable to false, signalizing a user logout.
+ * This way, any other page that is navigated to within the LDT app will have the information of whether or not
+ * a user has logged in successfully to get there.
+ *
+ * Creation Date: 8/10/16.
+ *
+ * @author Michael Kim
+ * @author Trevor Morse
  */
 session_start();
 $_SESSION['login'] = false;
