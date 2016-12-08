6fd1c97d789f6fa94cb8c403d872fe11976014f2
Stirling Algermissen <Stirling.S.Algermissen@jpl.nasa.gov>
2016-12-08 14:02:14 -0800

    Added license to scripts and created LICENSE.md

--- config/config.js
+++ config/config.js
@@ -1,4 +1,18 @@
 /**
+ * Copyright 2017 California Institute of Technology, developed with support from NASA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * @file Contains global objects and lists that are referenced throughout
  * the project. This file provides a central place for tracking this data.
  *
@@ -6,6 +20,7 @@
  *
  * @author Trevor Morse
  * @author Michael Kim
+ * @author Stirling Algermissen
  */
 /**
  * This object is for storing the paths to the various JSON files for PDS4 and
--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,25 @@
 /**
+ * Copyright 2017 California Institute of Technology, developed with support from NASA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * @file Contains the calls to initialize the LDT wizard and control its height.
  *
  * Creation Date: 6/17/16.
  *
  * @author Trevor Morse
  * @author Michael Kim
+ * @author Stirling Algermissen
  */
 $(document).ready(function(){
     matchWizardHeight($("#wizard .content.clearfix")[0], $("#wizard .actions.clearfix")[0],
