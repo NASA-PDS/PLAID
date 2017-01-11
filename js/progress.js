3b899eba42ccd6d466bd18a2cea719079fa57b39
Stirling Algermissen <Stirling.S.Algermissen@jpl.nasa.gov>
2017-01-11 02:54:51 +0000

    Fix discipline nodes revert bug

--- js/progress.js
+++ js/progress.js
@@ -37,6 +37,9 @@ function storeProgress(priorIndex, stepType){
         case "product_type":
             storeProductType(priorIndex, currObj);
             break;
+        case "discipline_nodes":
+            storeDisciplineNodes(priorIndex, currObj);
+            break;
         case "discipline_dictionaries":
             storeDisciplineNodes(priorIndex, currObj);
             break;
