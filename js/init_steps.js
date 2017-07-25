eacb878465dfd370b593bcfbd7f9d37e355d25a2
munn <Michael.L.Munn@jpl.nasa.gov>
2017-07-25 11:09:08 -0700

    Created a JQuery plug-in to allow the user to add code to set the default value of dropdown lists, and called its function.

--- js/init_steps.js
+++ js/init_steps.js
@@ -30,25 +30,6 @@
  * Note: Several aspects of the wizard are controlled within functions called in the
  * onStepChanging and onStepChanged methods of the wizard's settings object.
  */
-const IDENTIFICATION_AREA_PRODUCT_CLASS_DROPDOWN_ID = "0001_NASA_PDS_1.pds.Identification_Area.pds.product_class";
-const IDENTIFICATION_AREA_INFO_MODEL_VERSION_DROPDOWN_ID = "0001_NASA_PDS_1.pds.Identification_Area.pds.information_model_version";
-const PRODUCT_TYPE_OBSERVATIONAL_ID = "0001_NASA_PDS_1.pds.Product_Observational",
-      PRODUCT_TYPE_DOCUMENT_ID = "0001_NASA_PDS_1.pds.Product_Document",
-      PRODUCT_TYPE_CONTEXT_ID = "0001_NASA_PDS_1.pds.Product_Context",
-      PRODUCT_TYPE_FILE_TEXT_ID = "0001_NASA_PDS_1.pds.Product_File_Text",
-      PRODUCT_TYPE_THUMBNAIL_ID = "0001_NASA_PDS_1.pds.Product_Thumbnail";
-const PRODUCT_CLASS_OBSERVATIONAL = "Product_Observational",
-      PRODUCT_CLASS_DOCUMENT = "Product_Document",
-      PRODUCT_CLASS_CONTEXT = "Product_Context",
-      PRODUCT_CLASS_FILE_TEXT = "Product_File_Text",
-      PRODUCT_CLASS_THUMBNAIL = "Product_Thumbnail";
-const SCHEMA_VERSION_1800 = "1800",
-      SCHEMA_VERSION_1700 = "1700",
-      SCHEMA_VERSION_1600 = "1600";
-const SCHEMA_VERSION_DOTTED_1800 = "1.8.0.0",
-      SCHEMA_VERSION_DOTTED_1700 = "1.7.0.0",
-      SCHEMA_VERSION_DOTTED_1600 = "1.6.0.0";
-
 function initWizard(wizard) {
     var settings = {
         /* Appearance */
