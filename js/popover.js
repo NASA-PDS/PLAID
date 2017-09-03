66dd15410676748ddf449be69b4886bdc430a952
Kae Sawada <kae.sawada@jpl.nasa.gov>
2017-09-03 08:09:17 -0700

    Add static popover (Table Upload/Download button group)

--- js/popover.js
+++ js/popover.js
@@ -25,6 +25,7 @@
  */
 $(function() {
     initPopovers();
+    initStaticPopovers()
 });
 
 /**
@@ -68,6 +69,20 @@ function initPopovers(){
             html: true,
             title: title,
             content: dict[key],
+            trigger: "hover"
+        });
+    });
+}
+/**
+ * For each element with the class name "staticPopOver", initialize a popover.
+ */
+function initStaticPopovers(){
+    $('.staticPopover').each(function(){
+        $(this).popover({
+            container: "body",
+            html: true,
+            title: "static title",
+            content: "static content",
             trigger: "hover"
         });
     });
@@ -137,6 +152,42 @@ function addPopover(element, data, min, max, isRecommended){
         }, 0);
     });
 }
+/**
+ * Called to add a static popover.
+ * Uses data from the corresponding objects.
+ * @param {Element} element to add the popover to
+ * @param {Object} data object containing info for the popover
+ * @param {string} min info from element denoting minimum occurrences of the object
+ * @param {string} max info from element denoting maximum occurrences of the object
+ */
+// function addStaticPopover(element, data){
+//
+//     var title = data["title"].replace(/_/g, " ");
+//     var description = data["description"];
+//
+//     $(element).popover({
+//         container: "body",
+//         html: true,
+//         title: title,
+//         content: description,
+//         trigger: "manual",
+//         selector: true
+//
+//     }).on("mouseenter", function () {
+//         var _this = this;
+//         $(this).popover("show");
+//         $(".popover").on("mouseleave", function () {
+//             $(_this).popover('hide');
+//         });
+//     }).on("mouseleave", function () {
+//         var _this = this;
+//         setTimeout(function () {
+//             if (!$(".popover:hover").length) {
+//                 $(_this).popover("hide");
+//             }
+//         }, 0);
+//     });
+// }
 /**
 * Sometimes popovers remain on the page after the user has stopped hovering
 * over the corresponding element. This will hide all remaining popovers.
