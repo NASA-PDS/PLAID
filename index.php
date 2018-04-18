e6880450e2200b11339b5c2f91ad268ae4021af8
Stirling S Algermissen <stirling.s.algermissen@jpl.nasa.gov>
2018-04-18 10:20:07 -0700

    Fix for index.php not using correct session handler

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
     <form class="form-login" action="php/interact_db.php" method="post">
         <h2 class="form-login-heading">PDS Label Assistant for Interactive Design (PLAID)</h2>
         <?php
+        include_once("php/PlaidSessionHandler.php");
+        $session_handler = new PlaidSessionHandler();
         //  Enable the use of Session variables
         session_start();
         //  IF the error code is not empty
